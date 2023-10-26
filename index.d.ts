418f8ef600f2e10b70a88154bea04d792368c128
José F. Romaniello <jfromaniello@gmail.com>
2023-10-26 13:02:42 -0300

    fix type issue - closes #68

--- index.d.ts
+++ index.d.ts
@@ -29,7 +29,7 @@ declare interface SelfsignedOptions {
    * generate client cert signed by the original key
    * @default false
    */
-  clientCertificate?: undefined
+  clientCertificate?: boolean
   /**
    * client certificate's common name
    * @default "John Doe jdoe123"
